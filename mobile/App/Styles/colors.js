b238dc3fcfa001cdd4303fa6842630e9f06a8a41
everett980 <everett980@gmail.com>
2016-04-02 03:58:51 -0400

    i do not know

---
+++ mobile/App/Styles/colors.js
@@ -0,0 +1,4 @@
+export const BLUE = '#19B5FE';
+export const GREEN = '#21C064';
+export const GREY = '#969696';
+export const LIGHT_GREY = '#DADADA';
---
+++ mobile/App/Styles/index.js
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+import {
+  BLUE,
+  GREEN,
+  GREY,
+  LIGHT_GREY
+} from './colors';
+
+const createButtonStyle = (color) => ({
+  borderColor: color,
+  borderWidth: 2,
+  color: color,
+  fontSize: 16,
+  fontWeight: '600',
+  letterSpacing: 1,
+  paddingBottom: 15,
+  paddingLeft: 30,
+  paddingRight: 30,
+  paddingTop: 15,
+});
+
+export default StyleSheet.create({
+  blueButton: createButtonStyle(BLUE),
+  center: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  centerSelf: {
+    alignSelf: 'center',
+  },
+  fullWidth: {
+    flex: 1,
+  },
+  h1: {
+    color: GREY,
+    fontSize: 24,
+    letterSpacing: 0.5,
+  },
+  input: {
+    alignSelf: 'center',
+    borderWidth: 2,
+    borderColor: GREY,
+    height: 50,
+    textAlign: 'center',
+    width: 300,
+  },
+  logo: {
+    marginBottom: 20,
+  },
+  greenButton: createButtonStyle(GREEN),
+  test: {
+    borderColor: 'black',
+    borderWidth: 5,
+  }
+});
