b7ea9a73f82ce0dea8b3f81fcca8dff57730ece6
everett980 <everett980@gmail.com>
2016-04-03 08:52:59 -0400

    personalized more

--- browser/src/components/InfoBar/InfoBar.js
+++ browser/src/components/InfoBar/InfoBar.js
@@ -14,17 +14,8 @@ export default class InfoBar extends Component {
 
   render() {
     const {info, load} = this.props; // eslint-disable-line no-shadow
-    const styles = require('./InfoBar.scss');
     return (
-      <div className={styles.infoBar + ' well'}>
-        <div className="container">
-          This is an info bar
-          {' '}
-          <strong>{info ? info.message : 'no info!'}</strong>
-          <span className={styles.time}>{info && new Date(info.time).toString()}</span>
-          <button className="btn btn-primary" onClick={load}>Reload from server</button>
-        </div>
-      </div>
+			<div/>
     );
   }
 }
